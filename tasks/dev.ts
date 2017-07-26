dfe63383dfafa2583d762e00e5c72fa6e61fbb08
Diego Cordon Urbiola <dcordon@hiberus.local>
2017-07-26 13:36:58 +0200

    Always compile before running dev server

--- tasks/dev.ts
+++ tasks/dev.ts
@@ -1,7 +1,7 @@
 import * as gulp from 'gulp'
 import * as server from 'gulp-server-livereload'
 
-gulp.task('dev', ['watch'], () => {
+gulp.task('dev', ['compile', 'watch'], () => {
     
     gulp.src('app')
         .pipe(server({
