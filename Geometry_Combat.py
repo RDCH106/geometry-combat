048f32f668fc44f89f7fc9adec89a624512be444
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-07-28 01:43:28 +0200

    Improved Geometry_Combat.py

--- Geometry_Combat.py
+++ Geometry_Combat.py
@@ -1,16 +1,35 @@
 # -*- coding: utf-8 -*-
 
 import http.server
+import threading
+import webbrowser
+import os
 
 HOST = "0.0.0.0"
 PORT = 8000
 
-# Only share app folder
-web_dir = os.path.join(os.path.dirname(__file__), 'app')
-os.chdir(web_dir)
+def run_server():
 
-Handler = http.server.SimpleHTTPRequestHandler
+    # Only share app folder
+    web_dir = os.path.join(os.path.dirname(__file__), 'app')
+    print(web_dir)
+    os.chdir(web_dir)
 
-httpd = http.server.HTTPServer((HOST, PORT), Handler)
-print("Serving HTTP on %s port %s ..." % (HOST,PORT))
-httpd.serve_forever()
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    httpd = http.server.HTTPServer((HOST, PORT), Handler)
+    print("Serving HTTP on %s port %s ..." % (HOST,PORT))
+    httpd.serve_forever()
+    
+def launch_browser():
+    def open_browser_call():
+        # Open URL in a new tab, if a browser window is already open.
+        webbrowser.open_new_tab('http://localhost:8000/')
+
+    th = threading.Timer(interval=5, function=open_browser_call)  # Wait 5 seconds
+    th.start()
+
+
+if __name__ == "__main__":
+    launch_browser()
+    run_server()
