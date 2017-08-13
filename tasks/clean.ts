4310bbfe3fbd79f99affa671a51bfadd3d5ec4da
Diego Cordón <dcordonu@gmail.com>
2017-08-13 14:19:24 +0200

    Refactor clean task

--- tasks/clean.ts
+++ tasks/clean.ts
@@ -1,9 +1,19 @@
 import * as gulp from 'gulp'
 import * as del from 'del'
+import * as concat from 'lodash.concat'
+
+const includes = [
+    'app/assets/**',
+    'app/js/**'
+]
+
+const excludes = [
+    '!app/assets',
+    '!app/js'
+]
+
+const deleteOpts = concat([], includes, excludes)
 
 gulp.task('clean', () => {
-    return del([
-        'app/assets/**',
-        'app/js/**'
-    ])
+    return del(deleteOpts)
 })
